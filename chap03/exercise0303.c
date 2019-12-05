4e9d19e02cdf7e73e929168deae8730cbcc4041b
aocha <ao.hk215@gmail.com>
2019-12-05 13:00:25 +0900

    Add exercise 3-3

---
+++ chap03/exercise0303.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int no;
+
+  printf("整数を入力してください：");
+  scanf("%d", &no);
+
+  if (no > 0)
+    printf("絶対値は%dです。\n", no);
+  else
+    printf("絶対値は%dです。\n", -no);
+
+  return 0;
+}
