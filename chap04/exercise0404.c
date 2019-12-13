065141baad7f77a2034afbfaca1727d1a0127c17
aocha <ao.hk215@gmail.com>
2019-12-13 12:43:23 +0900

    Add exercise 4-4

---
+++ chap04/exercise0404.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int no;
+
+  printf("正の整数を入力してください：");
+  scanf("%d", &no);
+
+  while (no > 0)
+  {
+    printf("%d ", no--);
+
+    if (no == 0)
+      printf("\n");
+  }
+
+  return 0;
+}
