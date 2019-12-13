04e6ac251849f3b85c3a409a4d8eaeb49ad57817
aocha <ao.hk215@gmail.com>
2019-12-13 12:57:32 +0900

    Add exercise 4-8

---
+++ chap04/exercise0408.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int no;
+
+  printf("正の整数：");
+  scanf("%d", &no);
+
+  while (no-- > 0)
+  {
+    putchar('*');
+
+    if (no == 0)
+      putchar('\n');
+  }
+
+  return 0;
+}
