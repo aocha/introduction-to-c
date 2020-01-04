6ee4c3bb3f82a65b2c4258df9452dd424e539d31
aocha <ao.hk215@gmail.com>
2020-01-04 09:13:15 +0900

    Add exercise 5-12

---
+++ chap05/exercise0512.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int i, j, k;
+  int tensu[2][4][3] = {{{91, 63, 78}, {67, 72, 46}, {89, 34, 53}, {32, 54, 34}},
+                        {{97, 67, 82}, {73, 43, 46}, {97, 56, 21}, {85, 46, 35}}};
+  int sum[4][3] = {};
+
+  for (i = 0; i < 2; i++)
+  {
+    printf("%d回目の点数\n", i + 1);
+    for (j = 0; j < 4; j++)
+    {
+      for (k = 0; k < 3; k++)
+      {
+        printf("%4d", tensu[i][j][k]);
+      }
+      putchar('\n');
+    }
+  }
+
+  for (i = 0; i < 2; i++)
+  {
+    for (j = 0; j < 4; j++)
+    {
+      for (k = 0; k < 3; k++)
+      {
+        sum[j][k] += tensu[i][j][k];
+      }
+    }
+  }
+
+  puts("合計点");
+  for (i = 0; i < 4; i++)
+  {
+    for (j = 0; j < 3; j++)
+    {
+      printf("%4d", sum[i][j]);
+    }
+    putchar('\n');
+  }
+
+  return 0;
+}
