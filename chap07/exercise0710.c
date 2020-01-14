be9faf7f687cbb3df2bbc393447751afea7fceb7
aocha <ao.hk215@gmail.com>
2020-01-14 23:53:51 +0900

    Add exercise 7-10

---
+++ chap07/exercise0710.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int i;
+  float x = 0;
+
+  for (i = 0; i <= 100; i++)
+  {
+    printf("x = %f x = %f\n", x, i / 100.0);
+    x += 0.01;
+  }
+
+  return 0;
+}
